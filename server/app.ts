4a585fdd1c05b993eb16f4001511bdb7f88d4393
MyatSuMon253 <myatsumon2531999@gmail.com>
2025-07-12 18:43:14 +0630

    feat: define type definitions for room type

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv';
 import express from "express";
 import { dbConnect } from './config/dbConnect';
+import { startApolloServer } from './apollo/apolloServer';
 dotenv.config({ path: "config/.env.local" })
 
 const app = express();
 app.use(express.json())
 
 dbConnect();
+startApolloServer(app);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
---
+++ server/typeDefs/room.ts
@@ -0,0 +1,16 @@
+import gql from "graphql-tag";
+
+export const roomTypeDefs = gql`
+  type Room {
+    id: ID!
+    roomNumber: String!
+    type: String!
+    pricePerNight: Float!
+    capacity: Int!
+    isAvailable: Boolean!
+  }
+
+  type Query{}
+
+  type Mutation{}
+`;
