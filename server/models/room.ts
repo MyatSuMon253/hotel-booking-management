92bc66c4839e9ea8ae0cc466501c548ea102b3b4
MyatSuMon253 <myatsumon2531999@gmail.com>
2026-04-05 13:09:16 +0630

    feat: add review model

--- server/models/room.ts
+++ server/models/room.ts
@@ -1,51 +1,77 @@
 import mongoose from "mongoose";
 
-const roomSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please enter room title."],
+const roomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please enter room title."],
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter room description."],
+    },
+    roomNumber: {
+      type: String,
+      required: [true, "Please enter room number."],
+    },
+    type: {
+      type: String,
+      required: [true, "Please enter room type."],
+    },
+    pricePerNight: {
+      type: Number,
+      required: [true, "Please enter price per night."],
+    },
+    location: {
+      type: String,
+      required: [true, "Please enter location."],
+    },
+    capacity: {
+      type: Number,
+      required: [true, "Please enter room capacity."],
+    },
+    isAvailable: {
+      type: Boolean,
+      required: [true, "Please enter room availability."],
+      default: true,
+    },
+    images: {
+      type: [
+        {
+          url: String,
+          public_id: String,
+        },
+      ],
+    },
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "Review" }],
   },
-  description: {
-    type: String,
-    required: [true, "Please enter room description."],
+  {
+    timestamps: true,
   },
-  roomNumber: {
-    type: String,
-    required: [true, "Please enter room number."],
-  },
-  type: {
-    type: String,
-    required: [true, "Please enter room type."],
-  },
-  pricePerNight: {
-    type: Number,
-    required: [true, "Please enter price per night."],
-  },
-  location: {
-    type: String,
-    required: [true, "Please enter location."],
-  },
-  capacity: {
-    type: Number,
-    required: [true, "Please enter room capacity."],
-  },
-  isAvailable: {
-    type: Boolean,
-    required: [true, "Please enter room availability."],
-    default: true,
-  },
-  images: {
-    type: [{
-      url: String,
-      public_id: String,
-    }],
-  },
-  reviews: {
-    type: [String],
-  },
-}, {
-  timestamps: true
-})
+);
+
+roomSchema.virtual("ratings").get(function () {
+  let numOfReviews = this.reviews.length;
+
+  if (numOfReviews === 0) {
+    return {
+      value: 5,
+      count: 0,
+    };
+  }
+
+  const totalRatings = this.reviews.reduce(
+    (sum: number, review: any) => sum + review.rating,
+    0,
+  );
+
+  const value = numOfReviews > 0 ? totalRatings / numOfReviews : 0;
+
+  return {
+    value,
+    count: numOfReviews,
+  };
+});
 
 const Room = mongoose.model("Room", roomSchema);
 
